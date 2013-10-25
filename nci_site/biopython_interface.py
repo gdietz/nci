5f63bf772779f7d09289e6f7ea044ab1af31ea2c
George Dietz <dietzgeorge@gmail.com>
2013-10-24 22:29:18 -0400

    more django

---
+++ nci_site/biopython_interface.py
@@ -0,0 +1,10 @@
+################################################
+#
+# Description: Interface between the rest of the site and Biopython/Entrez
+#
+# Author: George Dietz
+#
+###############################################
+
+import Bio
+
